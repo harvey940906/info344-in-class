60cf584f1284662b265bf9cd60f932752dee526b
Dave Stearns <dlsinfo@uw.edu>
2017-04-19 14:29:40 -0700

    midway through class

--- tasksvr/handlers/tasks.go
+++ tasksvr/handlers/tasks.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"path"
+
 	"github.com/info344-s17/info344-in-class/tasksvr/models/tasks"
 )
 
@@ -32,9 +34,47 @@ func (ctx *Context) HandleTasks(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add(headerContentType, contentTypeJSONUTF8)
 		encoder := json.NewEncoder(w)
 		encoder.Encode(task)
+	case "GET":
+		tasks, err := ctx.TasksStore.GetAll()
+		if err != nil {
+			http.Error(w, "error getting tasks: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Add(headerContentType, contentTypeJSONUTF8)
+		encoder := json.NewEncoder(w)
+		encoder.Encode(tasks)
 	}
 }
 
 //HandleSpecificTask will handle requests for the /v1/tasks/some-task-id resource
 func (ctx *Context) HandleSpecificTask(w http.ResponseWriter, r *http.Request) {
+	_, id := path.Split(r.URL.Path)
+
+	switch r.Method {
+	case "GET":
+		task, err := ctx.TasksStore.Get(id)
+		if err != nil {
+			http.Error(w, "error finding task: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		w.Header().Add(headerContentType, contentTypeJSONUTF8)
+		encoder := json.NewEncoder(w)
+		encoder.Encode(task)
+	case "PATCH":
+		decoder := json.NewDecoder(r.Body)
+		task := &tasks.Task{}
+		if err := decoder.Decode(task); err != nil {
+			http.Error(w, "error decoding JSON: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		task.ID = id
+
+		if err := ctx.TasksStore.Update(task); err != nil {
+			http.Error(w, "error updating: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte("update successful!"))
+	}
+
 }
